bde65acae61da41dcbe4d3d961d38082a5f275fd
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-02 14:40:34 +0200

    fix_task

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -22,20 +22,15 @@ if (index !== -1) {
 }
 console.log(teachers);
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = [teachers[6]]
-teachers.pop()
-console.log(teachers);
-
+const lastTeacher = teachers.slice(0, 6);
 console.log(lastTeacher);
 
-
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher =[teachers[0]];
-
-teachers.shift()
-console.log(teachers);
+const firstTeacher = teachers.splice(1);
 
 console.log(firstTeacher);
+
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push('vanessa')
