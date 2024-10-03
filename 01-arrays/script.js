9f021d494ec5f789a52c1f4082588eecec04c83a
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-03 09:20:40 +0200

    fix_task

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,16 +10,15 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = [teachers[3]];
+const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
-let index = teachers.indexOf('Carlo')
-if (index !== -1) {
-    teachers[index] = 'patrick';
-}
+teachers[4] = 'Patrick';
+console.log(teachers);
+
 console.log(teachers);
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.slice(0, 6);
@@ -42,7 +41,9 @@ console.log(teachers);
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes('Fabio');
+console.log(isFabioPresent);
+
 
 
 
@@ -57,5 +58,9 @@ const teachersString = teachers.join(",");
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = teachers.length;
-console.log(isTeachersEmpty);
+const isTeachersEmpty = teachers;
+if (teachers.length === 0){
+  console.log('this array is empty');
+}
+
+
